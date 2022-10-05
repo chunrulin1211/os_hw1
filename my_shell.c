a9dd905a4e9ff22275ffd82f81223d94d29e97ec
chunrulin1211 <circlelin7@gmail.com>
2022-10-05 16:22:38 +0800

    first commit

---
+++ my_shell.c
@@ -0,0 +1,13 @@
+#ifndef _POSIX_C_SOURCE
+	#define  _POSIX_C_SOURCE 200809L
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+	printf("hello\n");
+	return 0;
+}
